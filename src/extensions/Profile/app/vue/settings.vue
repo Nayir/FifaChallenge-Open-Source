76d68b660633d820e53af3b609f01ab1088723e5
Nayir CHAMI <nayir.chami@gmail.com>
2017-02-06 15:23:39 +0100

    Profile Page linked to Firebase

--- src/extensions/Profile/app/vue/settings.vue
+++ src/extensions/Profile/app/vue/settings.vue
@@ -15,8 +15,9 @@
             <a> Paramètres </a>
           </router-link>
       </ul>
+      <button v-on:click="UpdateData" v-bind:class="[saveClass]" class="uk-button uk-float-right" type="button" v-show="showSaveButton"> {{ saveState }} </button>
       <h1 id="idplayer" class="uk-heading-divider">Informations Générales</h1>
-      <div class="uk-alert-danger" uk-alert>
+      <div class="uk-alert-danger" uk-alert v-if="!profile.username">
         <a class="uk-alert-close" uk-close></a>
         <p>Si vide, votre nom public sera votre nom de compte Google.</p>
       </div>
@@ -24,7 +25,7 @@
         <div class="uk-margin">
             <label class="uk-form-label" for="form-horizontal-text">Nom Public</label>
             <div class="uk-form-controls">
-                <input class="uk-input" id="form-horizontal-text" type="text" placeholder="">
+                <input class="uk-input" id="form-horizontal-text" type="text" placeholder="" v-model="profile.username">
             </div>
         </div>
       </form>
@@ -35,22 +36,22 @@
         <div class="uk-margin">
             <div class="uk-form-label">Consoles Possédés</div>
             <div class="uk-form-controls uk-form-controls-text">
-              <label><input class="uk-checkbox" type="checkbox"> Playstation 4</label> <br />
-              <label><input class="uk-checkbox" type="checkbox"> Xbox One</label>
+              <label><input class="uk-checkbox" type="checkbox" v-model="profile.gamerinfo.playstationfour.owned" v-bind:true-value="true" v-bind:false-value="false"> Playstation 4</label> <br />
+              <label><input class="uk-checkbox" type="checkbox" v-model="profile.gamerinfo.xboxone.owned" v-bind:true-value="true" v-bind:false-value="false"> Xbox One</label>
             </div>
         </div>
-        <div class="uk-margin">
+        <div class="uk-margin" v-show="profile.gamerinfo.playstationfour.owned">
             <h2 class="uk-heading-bullet">Playstation Network</h2>
             <label class="uk-form-label" for="form-horizontal-text">ID en ligne</label>
             <div class="uk-form-controls">
-                <input class="uk-input" id="form-horizontal-text" type="text" placeholder="Votre ID en ligne Playstation Network">
+                <input class="uk-input" id="form-horizontal-text" type="text" placeholder="Votre ID en ligne Playstation Network" v-model="profile.gamerinfo.playstationfour.id">
             </div>
         </div>
-        <div class="uk-margin">
+        <div class="uk-margin" v-show="profile.gamerinfo.xboxone.owned">
             <h2 class="uk-heading-bullet">Xbox Live</h2>
             <label class="uk-form-label" for="form-horizontal-text">Gamertag</label>
             <div class="uk-form-controls">
-                <input class="uk-input" id="form-horizontal-text" type="text" placeholder="Votre Gamertag Xbox Live">
+                <input class="uk-input" id="form-horizontal-text" type="text" placeholder="Votre Gamertag Xbox Live" v-model="profile.gamerinfo.xboxone.id">
             </div>
         </div>
       </form>
@@ -60,25 +61,88 @@
 
 <script>
 import firebase from 'firebase'
+// import UIkit from 'uikit'
 
 export default {
   data () {
     return {
       user: '',
-      log: ''
+      log: '',
+      saveState: 'Sauvegarder',
+      saveClass: 'uk-button-primary',
+      showSaveButton: false,
+      profile: {
+        username: '',
+        gamerinfo: {
+          playstationfour: {
+            owned: false,
+            id: ''
+          },
+          xboxone: {
+            owned: false,
+            id: ''
+          }
+        }
+      }
     }
   },
   mounted () {
     var self = this
+    // Check if user is logged
     firebase.auth().onAuthStateChanged(function (user) {
       if (user) {
         self.user = user
         self.log = true
+        firebase.database().ref('/users/' + user.uid).once('value').then(function (snapshot) {
+          self.profile = snapshot.val().profile
+          self.showSaveButton = true
+          return console.log('data loaded')
+        })
       } else {
         self.user = []
         self.log = false
       }
     })
+  },
+  watch: {
+    // TODO Change this code for matching with firebase > Safer and better
+    // Whenever profile changes, this function will run
+    'profile.username': function () {
+      this.ResetSaveButton()
+    },
+    'profile.gamerinfo.playstationfour.owned': function () {
+      this.ResetSaveButton()
+    },
+    'profile.gamerinfo.playstationfour.id': function () {
+      this.ResetSaveButton()
+    },
+    'profile.gamerinfo.xbox.owned': function () {
+      this.ResetSaveButton()
+    },
+    'profile.gamerinfo.xbox.id': function () {
+      this.ResetSaveButton()
+    }
+  },
+  methods: {
+    UpdateData () {
+      // Get data from vue
+      var postData = this.profile
+      var uid = this.user.uid
+      // Updates
+      var updates = {}
+      updates['/users/' + uid + '/profile/'] = postData
+      // TODO Make it callback
+      // https://github.com/uikit/uikit/issues/2345 BUG UIkit.notification('Sauvegardé')
+      // Saving to firebase
+      firebase.database().ref().update(updates)
+      this.saveState = 'Sauvegardé'
+      this.saveClass = 'uk-button-secondary'
+      console.log('sauvegardé')
+    },
+    ResetSaveButton () {
+      this.saveState = 'Sauvegarder'
+      this.saveClass = 'uk-button-primary'
+    }
   }
 }
 </script>
